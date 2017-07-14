aeb8943c79aabc669f71df68191824985e5505b8
Robin Van den Broeck <robin.broeck@gmail.com>
2017-07-14 16:59:43 +0200

    Added css to webpack so you can actually run the app (Running does not work atm because of errors)

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
       {
         test: /\.tsx?/,
         use: "ts-loader"
+      },
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
